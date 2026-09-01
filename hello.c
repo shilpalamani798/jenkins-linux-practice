a75e7fec1f846d9f58dda16e0ca72da65d73b364
shilpalamani798 <shilpalamani798@gmail.com>
2026-09-01 08:48:19 +0000

    updated8 hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-     printf("This is one of the updated C code for Analysis purposes
+     printf("This is one of the most
+ updated C code for Analysis purposes
 \n");
     return 0;
 }
